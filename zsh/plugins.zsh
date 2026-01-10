f72e7ce7a2beddbf738f2f30562614359f5c1692
nduyhai <nduyhai@users.noreply.github.com>
2026-01-10 16:06:29 +0700

    feat: fix missing widgets

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,12 +6,26 @@ fi
 # fzf (lazy-load to avoid startup cost)
 fzf_loaded=0
 load_fzf() {
-  (( fzf_loaded )) && return
+  ((fzf_loaded)) && return
   fzf_loaded=1
   [[ -r ~/.fzf.zsh ]] && source ~/.fzf.zsh
 }
 
 # Default fzf keybindings
-bindkey '^R' load_fzf   # history search
-bindkey '^T' load_fzf   # file widget
+load_fzf_history() {
+  load_fzf
+  (($ + widgets[fzf - history - widget])) || return
+  zle fzf-history-widget
+}
 
+load_fzf_files() {
+  load_fzf
+  (($ + widgets[fzf - file - widget])) || return
+  zle fzf-file-widget
+}
+
+zle -N load_fzf_history
+zle -N load_fzf_files
+
+bindkey '^R' load_fzf_history # history search
+bindkey '^T' load_fzf_files   # file widget
